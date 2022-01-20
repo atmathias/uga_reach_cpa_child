96fee773330a1ee77c23185acc4849e41f155bbe
Anthony Twesigye <twesigye10@gmail.com>
2022-01-20 13:22:36 +0300

    Time checks

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -27,3 +27,31 @@ df_sample_data <- sf::st_read("inputs/cpa_child_settlement_host_samples.gpkg", q
 # output holder -----------------------------------------------------------
 
 logic_output <- list()
+
+# Time checks -------------------------------------------------------------
+
+# Time interval for the survey
+min_time_of_survey <- 20
+max_time_of_survey <- 120
+
+df_c_survey_time <-  check_survey_time(input_tool_data = df_tool_data, 
+                                       input_min_time = min_time_of_survey, 
+                                       input_max_time = max_time_of_survey)
+
+if(exists("df_c_survey_time")){
+  if(nrow(df_c_survey_time) > 0){
+    logic_output$df_c_survey_time <- df_c_survey_time
+  }
+}
+
+# check the time between surveys
+min_time_btn_surveys <- 5
+
+df_c_time_btn_survey <- check_time_interval_btn_surveys(input_tool_data = df_tool_data,
+                                                        input_min_time = min_time_btn_surveys)
+
+if(exists("df_c_time_btn_survey")){
+  if(nrow(df_c_time_btn_survey) > 0){
+    logic_output$df_c_time_btn_survey <- df_c_time_btn_survey
+  }
+}
