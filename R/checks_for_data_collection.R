9180ab7f2e0fd87e2c000e4e743dc45908bc4182
Anthony Twesigye <twesigye10@gmail.com>
2022-01-20 13:19:06 +0300

    read data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -5,3 +5,21 @@ library(lubridate)
 library(glue)
 
 source("R/support_functions.R")
+
+# read data 
+df_tool_data <- readxl::read_excel("inputs/Child_Protection_Assessment_Child_Tool_Jan2022.xlsx") %>% 
+  mutate(i.check.uuid = `_uuid`,
+         i.check.start_date = as_date(start),
+         i.check.enumerator_id = enumerator_id,
+         i.check.district_name = district_name,
+         i.check.point_number = point_number,
+         start = as_datetime(start),
+         end = as_datetime(end)) %>% 
+  filter(consent == "yes", respondent_age >= 18, i.check.start_date > as_date("2022-01-20"), 
+         !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE))
+  )
+
+df_survey <- readxl::read_excel("inputs/Child_Protection_Child_Tool.xlsx", sheet = "survey")
+df_choices <- readxl::read_excel("inputs/Child_Protection_Child_Tool.xlsx", sheet = "choices")
+
+df_sample_data <- sf::st_read("inputs/cpa_child_settlement_host_samples.gpkg", quiet = TRUE)
